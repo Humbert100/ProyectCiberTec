41417bbf1f821925c02b177445c245bb0f8828be
Misaelchavez16 <75141227+Misaelchavez16@users.noreply.github.com>
2022-10-09 12:02:48 -0500

    Cambios css img

--- static/scripts/procces_user_data.js
+++ static/scripts/procces_user_data.js
@@ -47,7 +47,7 @@ function process_login_info(){
 
 function process_sign_in_info(){
     var continue_next = true;
-    
+    /*
     switch(true){
         case(!("#user_name").val()):
             alert("Favor de ingresar su nombre");
@@ -81,7 +81,7 @@ function process_sign_in_info(){
     if(continue_next == false){
         console.log("usuario invalido para registrar");
         return;
-    }
+    }*/
 
     let name = ("#user_name").val() + " " + ("#user_lastname").val()
     let user_sign_up_data = {   'name'      :   name,
@@ -90,8 +90,8 @@ function process_sign_in_info(){
     console.log(user_sign_up_data)
 
     $.ajax({
-        type        :   "POST",
         url         :   "/user/create",
+        type        :   "POST",
         data        :   JSON.stringify(user_login_data),
         contentType :   'application/json',
         dataType    :   "json",
