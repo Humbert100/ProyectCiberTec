c3bc9ee5d87fed05810b8cacb3848eb6a8f9eab1
Misaelchavez16 <75141227+Misaelchavez16@users.noreply.github.com>
2022-10-14 15:11:19 -0500

    push 14/10

--- static/scripts/user_crud_func.js
+++ static/scripts/user_crud_func.js
@@ -1,4 +1,23 @@
 function new_div(obj){
-    let the = `<div class="single_row_user" id="${obj.id}">
-                <div><a href='#' style="text-decoration-color"`
+    let the = `    <div class="single_row_user" id="${obj.id}">
+    <div><a href='#' style="text-decoration-color: black;"><p id="row_user">${obj.id}</p></a></div>
+    <div><p id="row_email">${obj.email}</p></div>
+    <div><p id="row_name">${obj.name}</p></div>
+    <div>
+        <select id="row_admin" name="">
+            <option value="True">True</option>
+            <option value="True">False</option>
+        </select>
+    </div>
+    <div><p for="row_super_admin">${obj.superAdmin}</p></div>
+    <div>
+        <select name="" id="row_block">
+            <option value="${obj.block}">${obj.block}</option>
+            <option value="True">True</option>
+            <option value="False">False</option>
+        </select>
+    </div>
+    <div><p id="row_verified">${obj.verified}</p></div>
+</div>`;
+
 }
